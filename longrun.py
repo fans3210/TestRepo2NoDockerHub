8653921f1e099cc3396d3e4f09fe1437d854564f
fans3210 <liyaodong2@gmail.com>
2020-07-27 21:38:28 +0800

    commit

--- longrun.py
+++ longrun.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     cnt = 0
     total = 100
     while cnt <= total:
-        time.sleep(0.5)
+        time.sleep(0.25)
         print(f'current cnt = {cnt}, {total-cnt} to go')
         cnt += 1
 

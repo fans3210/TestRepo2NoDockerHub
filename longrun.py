bdd51015efb5c7de6ba919d0f222b9e27e25b325
fans3210 <liyaodong2@gmail.com>
2020-09-11 13:54:28 +0800

    commit

--- longrun.py
+++ longrun.py
@@ -4,21 +4,21 @@ import pandas as pd
 
 if __name__ == "__main__":
 
-    print('test env datasetinput = ')
-    print(os.getenv('DATASET_INPUT'))
-
-    print('test list file')
-    files = os.listdir('/shared/dataset/')
-    print(files)
-
-    df = pd.read_json(os.getenv('DATASET_INPUT'))
-    print(df)
-
     cnt = 0
     total = 100
     while cnt <= total:
         time.sleep(0.25)
+        print('check env DATASET_INPUT = ')
+        print(os.getenv('DATASET_INPUT'))
+        print('check env DATASET_OUTPUT = ')
+        print(os.getenv('DATASET_OUTPUT'))
 
+        print('list file in dataset path')
+        files = os.listdir('/shared/dataset/')
+        print(files)
+
+        df = pd.read_json(os.getenv('DATASET_INPUT'))
+        print(df)
         print(f'current cnt = {cnt}, {total-cnt} to go')
         cnt += 1
 

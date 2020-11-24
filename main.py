7c7deef52b8b4f920d5999922a5a359af3f65bf3
fans3210 <liyaodong2@gmail.com>
2020-11-24 13:05:03 +0800

    mainpy read envs

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 import numpy as np
 import pandas as pd
-
+import time
+import os
 
 if __name__ == "__main__":
-    print('hello world')
-    test_arr = np.array([1, 2, 3])
-    print('test_arr = ', test_arr)
-    test_df = pd.DataFrame(data=[1, 2, 3], columns=['Hi'])
-    print('test_df = ', test_df)
-    print('test 1')
+    print('check env vars:')
+    input_file = os.getenv('INPUT_FILE')
+    input_folder = os.getenv('INPUT_FOLDER')
+    output_file = os.getenv('OUTPUT_FILE')
+    output_folder = os.getenv('OUTPUT_FOLDER')
+
+    print(f'''
+    input file = {input_file}
+    output file = {output_file}
+    input folder = {input_folder}
+    output folder = {output_folder}
+    ''')
+
+    time.sleep(30)
